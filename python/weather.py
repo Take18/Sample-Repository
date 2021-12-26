b7ef986baf0db7a56f74feb4989b6d3a2ab1fc91
武智亮 <tomoaki@takechiryounoMacBook-Air.local>
2021-12-26 20:56:36 +0900

    解答例にコメント追加

--- python/weather.py
+++ python/weather.py
@@ -2,24 +2,36 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-url='https://weather.yahoo.co.jp/weather/'
-res=requests.get(url)
-soup=BeautifulSoup(res.text,"html.parser")
+def show_forecast(dl):
+    print(dl.dt.contents[0]+"の天気\n")
+    print(dl.dd.contents[1].img.attrs['alt'])
+    print("最高気温は"+dl.dd.contents[3].contents[0].contents[0]+"度")
+    print("最低気温は"+dl.dd.contents[3].contents[2].contents[0]+"度")
+    print("降水確率は"+dl.dd.contents[5].contents[0])
+
 
 if len(sys.argv) == 1:
     print("都市が指定されていません。")
     exit()
 city_name = sys.argv[1]
 
-city_names = soup.select("#map li.point > a > dl > dt.name")
+url='https://weather.yahoo.co.jp/weather/'
+res=requests.get(url)
+soup=BeautifulSoup(res.text,"html.parser")
 
 dl = False
+
+# 最初のページに指定された都市があるか確認
+city_names = soup.select("#map li.point > a > dl > dt.name")
 for city in city_names:
     if city_name == city.contents[0]:
         dl = city.parent
 
 if dl == False:
+    # 各ページのリンク取得
     hrefs = map(lambda a: a.attrs['href'], soup.select("#map li.point > a"))
+
+    # 取得したページの中に指定された都市があるか確認
     for href in hrefs:
         res=requests.get("https://weather.yahoo.co.jp"+href)
         soup=BeautifulSoup(res.text,"html.parser")
@@ -32,8 +44,4 @@ if dl == False:
     print("指定された都市が見つかりませんでした。")
     exit()
 
-print(dl.dt.contents[0]+"の天気\n")
-print(dl.dd.contents[1].img.attrs['alt'])
-print("最高気温は"+dl.dd.contents[3].contents[0].contents[0]+"度")
-print("最低気温は"+dl.dd.contents[3].contents[2].contents[0]+"度")
-print("降水確率は"+dl.dd.contents[5].contents[0])
+show_forecast(dl)
