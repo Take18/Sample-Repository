5e32812bc42122690f0e16362d1b26c5e7a01d40
str0930 <tanebe.satoru@gmail.com>
2022-02-12 14:51:42 +0900

    script.jsをコミット

--- calculator/script.js
+++ calculator/script.js
@@ -1,30 +1,41 @@
 $(function() {
-    let num_1 = '', num_2 = '', answer = '';
-    push_num = [],
+    let num_1 = '',
+        num_2 = '',
+        answer = '';
+        push_num = [],
         cal = '',
-        calculation = false;
+        calculation = false,
+        memo_num = 0;
 
     $('.number').on('click', function () {
+
+        $('#minus').text('-');
+
         push_num.push($(this).text());
         if (calculation == true) {
             num_2 = push_num.join('');
             $('#display').text(num_2);
+            console.log(num_2);
         } else {
             num_1 = push_num.join('');
             $('#display').text(num_1);
+            console.log(num_1);
         }
     });
 
     $('.operator').on('click', function () {
         cal = $(this).attr('id');
-        caluculation = true;
+        calculation = true;
         push_num = [];
     });
 
     $('#equal').on('click', function () {
 
-        let int_1 = parseInt(num_1);
-        let int_2 = parseInt(num_1 );
+        let int_1 = parseFloat(num_1);
+        console.log(int_1);
+        let int_2 = parseFloat(num_2);
+        console.log(int_2);
+
         switch (cal) {
 
             case 'add':
@@ -42,6 +53,9 @@ $(function() {
             case 'divide':
                 answer = int_1 / int_2;
                 break;
+
+            default:
+                answer = num_1;
         }
         $('#display').text(answer);
         num_1 = answer;
@@ -58,11 +72,17 @@ $(function() {
     });
 
     $('#memory').on('click', function () {
+        let dis_num_1 = $('#display').text();
+        let dis_num_2 = parseFloat(dis_num_1);
+        memo_num += dis_num_2;
     });
 
     $('#memory_clear').on('click', function () {
+        memo_num = 0;
     });
 
     $('#memory_recall').on('click', function () {
+        $('#display').text(memo_num);
+        num_1 = memo_num;
     });
 });
